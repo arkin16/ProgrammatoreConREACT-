a16244f405b9c23fc9e09b8d1bc854e73a51a9b3
Nik <niktridell@gmail.com>
2022-12-22 23:51:02 +0100

    Fix Button Info (regenerated)

--- lezione12/eserc_12/js/script.js
+++ lezione12/eserc_12/js/script.js
@@ -1,35 +1,54 @@
 let url = 'https://jsonplaceholder.typicode.com/users/';
-let prom = fetch(url).then(get => get.json());
-let usersA = [];
+let promise = fetch(url).then(get => get.json());
+let arrU = [];
+
+const div = document.getElementById("cont");
+const subTable = document.getElementById("subifo");
 
 document.addEventListener('DOMContentLoaded', () => {
-    prom.then(users => {
-        // console.log(users, users[0].name, users[0].username, users[0].email );
+    promise.then(resp => {
+        arrU = resp;
+        print()
     });
 });
 
-function viewContactList() {
-    // for (let i = 0; i < listaContatti.length; i++) { console.log(listaContatti[i]) }
-    // for (const key in listaContatti) { console.log(listaContatti[key]) }
-    // for (const contatto of listaContatti) { console.log(contatto) }
-
-    let lista = document.getElementsByClassName("list-group");
-
-    document.addEventListener('DOMContentLoaded', () => {
-        prom.then(users => {
-            users.forEach((c, i) => {
-                console.log("users " + users[i].name)
-               
-                let riga = document.createElement('li');
-                riga.className = 'list-group-item';
-                riga.innerHTML = "users " /* + users[i].name */ + ' <a class="btn btn-primary btn-sm collapsed" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Info</a>';
-
-
-                // console.log("riga "+riga)
-                lista.appendChild(riga);
-            })
-        });
-    });
-
+function print(){
+    div.innerHTML = pritListofUser(arrU);
 }
-viewContactList()
+
+function pritListofUser(arrU){
+    const users = arrU.map( (user) => ` 
+                                        <tr>
+                                            <td>${user.name}</td>
+                                            <td>${user.username}</td>
+                                            <td>${user.email}</td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary btn" onclick="setIndex(${user.id})">Info</button>
+                                            </td>
+                                            <td>
+                                                <button type="button" class="btn-close" aria-label="Close"></button>
+                                            </td>
+                                        </tr>`).join("");
+return `${users}`;                                        
+}
+
+function setIndex(id){
+    subTable.innerHTML = printInfoE(id);
+}
+
+function printInfoE(id){
+    let objtmp = arrU.find(obj => obj.id === id);
+
+    const subtable = ` 
+                         <tr>
+
+                             <td>${objtmp.name}</td>
+                             <td>${objtmp.address.city}</td>
+                             <td>${objtmp.address.street}</td>
+                             <td>${objtmp.phone}</td>
+
+                         <tr>`;    
+
+    return `${subtable}`;
+}
+
