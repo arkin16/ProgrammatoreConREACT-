040e011ca27c5dc9d9fd4900f32c633c5f89177e
Nik <niktridell@gmail.com>
2022-12-26 19:04:16 +0100

    Added delete user functionality

--- lezione12/eserc_12/js/script.js
+++ lezione12/eserc_12/js/script.js
@@ -1,45 +1,58 @@
-let url = 'https://jsonplaceholder.typicode.com/users/';
-let promise = fetch(url).then(get => get.json());
+let url = "https://jsonplaceholder.typicode.com/users/";
+let promise = fetch(url).then((get) => get.json());
 let arrU = [];
 
 const div = document.getElementById("cont");
 const subTable = document.getElementById("subifo");
 
-document.addEventListener('DOMContentLoaded', () => {
-    promise.then(resp => {
-        arrU = resp;
-        print()
-    });
+class Rubrica {
+  constructor(fullname, username, email, phone, city, street) {
+    this.fullname = fullname;
+    this.username = username;
+    this.email = email;
+    this.phone = phone;
+    this.city = city;
+    this.street = street;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  promise.then((resp) => {
+    arrU = resp;
+    print();
+  });
 });
 
-function print(){
-    div.innerHTML = pritListofUser(arrU);
+function print() {
+  div.innerHTML = pritListofUser(arrU);
 }
 
-function pritListofUser(arrU){
-    const users = arrU.map( (user) => ` 
-                                        <tr>
-                                            <td>${user.name}</td>
-                                            <td>${user.username}</td>
-                                            <td>${user.email}</td>
-                                            <td>
-                                                <button type="button" class="btn btn-primary btn" onclick="setIndex(${user.id})">Info</button>
-                                            </td>
-                                            <td>
-                                                <button type="button" class="btn-close" aria-label="Close"></button>
-                                            </td>
-                                        </tr>`).join("");
-return `${users}`;                                        
+function pritListofUser(arrU) {
+  const users = arrU
+    .map((user) => ` 
+                    <tr>
+                        <td>${user.name}</td>
+                        <td>${user.username}</td>
+                        <td>${user.email}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn" onclick="setIndex(${user.id})">Info</button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn-close" aria-label="Close" onclick="removeUser(${user.id})"></button>
+                        </td>
+                    </tr>`)
+                           .join("");
+  return `${users}`;
 }
 
-function setIndex(id){
-    subTable.innerHTML = printInfoE(id);
+function setIndex(id) {
+  subTable.innerHTML = printInfoE(id);
 }
 
-function printInfoE(id){
-    let objtmp = arrU.find(obj => obj.id === id);
+function printInfoE(id) {
+  let objtmp = arrU.find((obj) => obj.id === id);
 
-    const subtable = ` 
+  const subtable = ` 
                          <tr>
 
                              <td>${objtmp.name}</td>
@@ -47,8 +60,24 @@ function printInfoE(id){
                              <td>${objtmp.address.street}</td>
                              <td>${objtmp.phone}</td>
 
-                         <tr>`;    
+                         <tr>`;
 
-    return `${subtable}`;
+  return `${subtable}`;
 }
 
+
+
+
+
+
+
+
+function removeUser (id){
+    fetch(url+id, {method: "DELETE"})
+    .then(res => res.json())
+    .then(() => {
+        arrU = arrU.filter(user => user.id !== id);
+        print();
+    })
+    .catch(error => console.log(error))
+}
