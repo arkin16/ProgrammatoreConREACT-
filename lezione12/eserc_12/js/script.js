1900390737faf89eb2bf3d244b6ddb95418af812
Nik <niktridell@gmail.com>
2022-12-28 23:53:30 +0100

    Add class

--- lezione12/eserc_12/js/script.js
+++ lezione12/eserc_12/js/script.js
@@ -6,13 +6,15 @@ const div = document.getElementById("cont");
 const subTable = document.getElementById("subifo");
 
 class Rubrica {
-  constructor(fullname, username, email, phone, city, street) {
-    this.fullname = fullname;
+  constructor(name, username, email, phone, city, street) {
+    this.name = name;
     this.username = username;
     this.email = email;
     this.phone = phone;
-    this.city = city;
-    this.street = street;
+    this.address = {
+      street: street,
+      city: city,
+    };
   }
 }
 
@@ -28,20 +30,20 @@ function print() {
 }
 
 function pritListofUser(arrU) {
-  const users = arrU
-    .map((user) => ` 
-                    <tr>
-                        <td>${user.name}</td>
-                        <td>${user.username}</td>
-                        <td>${user.email}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn" onclick="setIndex(${user.id})">Info</button>
-                        </td>
-                        <td>
-                            <button type="button" class="btn-close" aria-label="Close" onclick="removeUser(${user.id})"></button>
-                        </td>
-                    </tr>`)
-                           .join("");
+  console.log(arrU);
+  const users = arrU.map((user) => `    <tr>
+                                            <td>${user.name}</td>
+                                            <td>${user.username}</td>
+                                            <td>${user.email}</td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary btn" onclick="setIndex(${user.id})">Info</button>
+                                            </td>
+                                            <td>
+                                                <button type="button" class="btn-close" aria-label="Close" onclick="removeUser(${user.id})"></button>
+                                            </td>
+                                        </tr>`
+    )
+    .join("");
   return `${users}`;
 }
 
@@ -65,19 +67,34 @@ function printInfoE(id) {
   return `${subtable}`;
 }
 
+document.getElementById("save").addEventListener("click", saveContact);
 
+function saveContact() {
+  let name = document.getElementById("fullname").value;
+  let username = document.getElementById("username").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let city = document.getElementById("city").value;
+  let street = document.getElementById("street").value;
 
+  const newUser = new Rubrica(name, username, email, phone, city, street);
 
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(newUser),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((res) => res.json())
+    .then((rest) => arrU.push(rest), print())
+    .catch((error) => console.log(error));
+}
 
-
-
-
-function removeUser (id){
-    fetch(url+id, {method: "DELETE"})
-    .then(res => res.json())
+function removeUser(id) {
+  fetch(url + id, { method: "DELETE" })
+    .then((res) => res.json())
     .then(() => {
-        arrU = arrU.filter(user => user.id !== id);
-        print();
+      arrU = arrU.filter((user) => user.id !== id);
+      print();
     })
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 }
